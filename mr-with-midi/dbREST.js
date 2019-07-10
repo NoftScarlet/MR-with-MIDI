8b3ecf0c0306f210c118d44c83255842cb26d871
Yixiao Chen <ychen239@uottawa.ca>
2019-07-10 18:21:51 -0400

    Adding GraphQL and REST parallel

---
+++ mr-with-midi/dbREST.js
@@ -0,0 +1,11 @@
+let intCPosBrk = require('./src/score/interval-C-position-broken.json');
+
+module.exports = function() {
+    return {
+        intCPosBrk: intCPosBrk,
+        intCPosBrk_header: intCPosBrk.header,
+        intCPosBrk_sessions: intCPosBrk.sessions
+    }
+};
+
+
